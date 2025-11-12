2e9f8a95027ba36997c4aafec3357c2df435e602
Awad_7 <awadfahem7@gmail.com>
2025-11-12 21:55:50 +0200

    Done

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,12 +1,22 @@
 import React, { useContext } from "react";
-import { assets} from "./../assets/assets";
+import { assets } from "./../assets/assets";
 import { PlayerContext } from "../content/PlayerContent.jsx";
 function Player() {
-  const { track, seekBar, seekBg,seekSong, playStatus, play, pause ,time , next , previous } =
-    useContext(PlayerContext);
+  const {
+    track,
+    seekBar,
+    seekBg,
+    seekSong,
+    playStatus,
+    play,
+    pause,
+    time,
+    next,
+    previous,
+  } = useContext(PlayerContext);
 
   return (
-    <div className="h-[10%] bg-black flex justify-between items-center text-white px-4">
+    <div className="h-[10vh] bg-black flex justify-between items-center text-white px-4">
       <div className="hidden lg:flex items-center gap-4">
         <img className="w-12" src={track.image} alt="" />
         <div>
@@ -22,7 +32,12 @@ function Player() {
             src={assets.shuffle_icon}
             alt=""
           />
-          <img onClick={previous} className="w-4 cursor-pointer" src={assets.prev_icon} alt="" />
+          <img
+            onClick={previous}
+            className="w-4 cursor-pointer"
+            src={assets.prev_icon}
+            alt=""
+          />
           {playStatus ? (
             <img
               onClick={pause}
@@ -38,13 +53,20 @@ function Player() {
               alt=""
             />
           )}
-          <img onClick={next} className="w-4 cursor-pointer" src={assets.next_icon} alt="" />
+          <img
+            onClick={next}
+            className="w-4 cursor-pointer"
+            src={assets.next_icon}
+            alt=""
+          />
           <img className="w-4 cursor-pointer" src={assets.loop_icon} alt="" />
         </div>
         <div className="flex items-center gap-5">
-          <p>{time.currentTime.minute}:{time.currentTime.second}</p>
+          <p>
+            {time.currentTime.minute}:{time.currentTime.second}
+          </p>
           <div
-          onClick={seekSong}
+            onClick={seekSong}
             ref={seekBg}
             className="w-[60vw] max-w-[500px] bg-gray-300 rounded-full cursor-pointer"
           >
@@ -53,7 +75,9 @@ function Player() {
               className="h-1 border-none w-10 bg-green-800 rounded-full"
             />
           </div>
-          <p>{time.totalTime.minute}:{time.totalTime.second}</p> 
+          <p>
+            {time.totalTime.minute}:{time.totalTime.second}
+          </p>
         </div>
       </div>
 
